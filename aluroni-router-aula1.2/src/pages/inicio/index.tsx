48e4af29308a9bec6b69c74c4f9202966e78ecbe
Leo-Schuch <leoschuch23@gmail.com>
2022-07-01 14:27:48 -0300

    Novo
    Atualizado

---
+++ aluroni-router-aula1.2/src/pages/inicio/index.tsx
@@ -0,0 +1,55 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import cardapio from 'data/cardapio.json';
+import styles from './Inicio.module.scss';
+import stylesTema from 'styles/Tema.module.scss';
+import nossaCasa from 'assets/nossa_casa.png';
+import { useNavigate } from 'react-router-dom';
+import { Prato } from 'types/Prato';
+
+export default function Inicio() {
+  let pratosRecomendados = [...cardapio];
+  pratosRecomendados = pratosRecomendados.sort(() => 0.5 - Math.random()).
+    splice(0, 3);
+
+  const navigate = useNavigate();
+
+  function redirecionarParaDetalhes(prato: Prato) {
+    navigate(`/prato/${prato.id}`, { state: { prato }, replace: true });
+
+  }
+  return (
+    <section>
+
+
+      <h3 className={stylesTema.titulo}>
+        Recomendações da cozinha
+      </h3>
+      <div className={styles.recomendados}>
+        {pratosRecomendados.map(item => (
+          <div key={item.id} className={styles.recomendado}>
+            <div className={styles.recomendado__imagem}>
+              <img src={item.photo} alt={item.title} />
+
+            </div>
+            <button
+              className={styles.recomendado__botao}
+
+              onClick={() => redirecionarParaDetalhes(item)}
+            >
+              ver mais
+            </button>
+
+          </div>
+
+        ))}
+      </div>
+      <h3 className={stylesTema.titulo}>Nossa casa</h3>
+      <div className={styles.nossaCasa}>
+        <img src={nossaCasa} alt="Abelhas house" />
+        <div className={styles.nossaCasa__endereco}>
+          General Ayrosa. Itaipava - Petrópolis <br></br>
+        </div>
+      </div>
+    </section>
+  );
+}
